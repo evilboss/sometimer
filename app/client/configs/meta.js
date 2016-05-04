ed813264004dc23f267ed85a01732dcbac9dbee0
aaron <aaronrandrup@gmail.com>
2016-05-04 14:10:43 +0800

    added lodash package

--- app/client/configs/meta.js
+++ app/client/configs/meta.js
@@ -3,8 +3,8 @@ DocHead.setTitle(title);
 var metaInfo = [
   {name: "description", content: "Remotiv Platform"},
   {name: "viewport", content: "user-scalable=no, initial-scale=1, minimal-ui, maximum-scale=1, minimum-scale=1"}
-
 ];
+
 _.each(metaInfo, function (metaItem) {
   DocHead.addMeta(metaItem);
 });

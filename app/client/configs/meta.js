c6cd4f0278352e09e63fd2d29b1f28538aea40fd
aaron <aaronrandrup@gmail.com>
2016-05-04 15:12:34 +0800

    added materialize icons in meta

--- app/client/configs/meta.js
+++ app/client/configs/meta.js
@@ -1,10 +1,13 @@
 var title = "Remotiv-platform";
 DocHead.setTitle(title);
+/*meta data*/
 var metaInfo = [
   {name: "description", content: "Remotiv Platform"},
   {name: "viewport", content: "user-scalable=no, initial-scale=1, minimal-ui, maximum-scale=1, minimum-scale=1"}
 ];
-
 _.each(metaInfo, function (metaItem) {
   DocHead.addMeta(metaItem);
 });
+/*head links*/
+var linkInfo = {rel: "stylesheet", href: "https://fonts.googleapis.com/icon?family=Material+Icons"};
+DocHead.addLink(linkInfo);

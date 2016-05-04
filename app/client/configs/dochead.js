22c68c6b9744cd58af2423186a2b448eaa5589e3
aaron <aaronrandrup@gmail.com>
2016-05-04 11:20:33 +0800

    added viewport meta data for screen scaling

--- app/client/configs/dochead.js
+++ app/client/configs/dochead.js
@@ -1,2 +1,10 @@
 var title = "Remotiv-platform";
 DocHead.setTitle(title);
+var metaInfo = [
+  {name: "description", content: "Remotiv Platform"},
+  {name: "viewport", content: "user-scalable=no, initial-scale=1, minimal-ui, maximum-scale=1, minimum-scale=1"}
+
+];
+_.each(metaInfo, function (metaItem) {
+  DocHead.addMeta(metaItem);
+});

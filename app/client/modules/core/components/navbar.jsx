fa58fe8038f9931ab714ad98ae96056468f42ca7
aaron <aaronrandrup@gmail.com>
2016-05-10 14:33:09 +0800

    partial responsive header

--- app/client/modules/core/components/navbar.jsx
+++ app/client/modules/core/components/navbar.jsx
@@ -10,12 +10,20 @@ class Navbar extends React.Component {
   }
 
   render() {
-
     return (
+
       <nav>
         <div className="nav-wrapper cyan">
           <a href="#!" className="brand-logo">Remotiv-App</a>
+          <a href="#" data-activates="mobile-demo" className="button-collapse"><i
+            className="material-icons">menu</i></a>
           <ul className="right hide-on-med-and-down">
+            <li><a href=""><i className="mdi-social-notifications"></i></a></li>
+            <li><a href=""><i className="material-icons">view_module</i></a></li>
+            <li><a href=""><i className="material-icons">refresh</i></a></li>
+            <li><a href="" onClick={this.toggleFullscreen}><i className="material-icons">settings_overscan</i></a></li>
+          </ul>
+          <ul className="side-nav" id="mobile-demo">
             <li><a href=""><i className="mdi-social-notifications"></i></a></li>
             <li><a href=""><i className="material-icons">view_module</i></a></li>
             <li><a href=""><i className="material-icons">refresh</i></a></li>

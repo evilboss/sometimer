e65875bdd470c701d3efc0f7696362d3f3d67e52
aaron <aaronrandrup@gmail.com>
2016-05-10 15:16:51 +0800

    added sidenav test imported sidenav in navbar

--- app/client/modules/core/components/navbar.jsx
+++ app/client/modules/core/components/navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Screenfull from 'screenfull';
-
+import SideNav from './side_nav.jsx';
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.toggleFullscreen = ()=> {
       Screenfull.enabled ? Screenfull.toggle() : void 0;
     };
-  }
+    this.componentDidMount = ()=> {
+      $(".button-collapse").sideNav();
+    };
+  };
+
 
   render() {
     return (
@@ -15,7 +19,7 @@ class Navbar extends React.Component {
       <nav>
         <div className="nav-wrapper cyan">
           <a href="#!" className="brand-logo">Remotiv-App</a>
-          <a href="#" data-activates="mobile-demo" className="button-collapse"><i
+          <a href="#" data-activates="mobile-mode" className="button-collapse"><i
             className="material-icons">menu</i></a>
           <ul className="right hide-on-med-and-down">
             <li><a href=""><i className="mdi-social-notifications"></i></a></li>
@@ -23,12 +27,7 @@ class Navbar extends React.Component {
             <li><a href=""><i className="material-icons">refresh</i></a></li>
             <li><a href="" onClick={this.toggleFullscreen}><i className="material-icons">settings_overscan</i></a></li>
           </ul>
-          <ul className="side-nav" id="mobile-demo">
-            <li><a href=""><i className="mdi-social-notifications"></i></a></li>
-            <li><a href=""><i className="material-icons">view_module</i></a></li>
-            <li><a href=""><i className="material-icons">refresh</i></a></li>
-            <li><a href="" onClick={this.toggleFullscreen}><i className="material-icons">settings_overscan</i></a></li>
-          </ul>
+          <SideNav />
         </div>
       </nav>
     );

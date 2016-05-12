451ed2abbba829ddb794590fe8a983b409a0f01c
aaron <aaronrandrup@gmail.com>
2016-05-12 11:09:45 +0800

    REM-114 added mupx json

--- app/client/modules/core/components/side_nav.jsx
+++ app/client/modules/core/components/side_nav.jsx
@@ -3,20 +3,22 @@ import React from 'react';
 class SideNav extends React.Component {
   constructor(props) {
     super(props);
+    
   }
+
 // TODO:move sidenav to right
   render() {
     return (
-
-      <ul id="nav-mobile" className="side-nav right hide-on-large-only">
-        <li><a href="">
-          <i className="mdi-communication-email"></i>
-          <span>Mailbox</span>
-        </a></li>
-        <li><a href=""><i className="material-icons">view_module</i></a></li>
-        <li><a href=""><i className="material-icons">refresh</i></a></li>
-        <li><a href="" onClick={this.toggleFullscreen}><i className="material-icons">settings_overscan</i></a></li>
-      </ul>
+      <section id="side-nav">
+        <ul id="nav-mobile" className="side-nav fixed right hide-on-large-only">
+          <li className="bold">
+            <a href="" className="waves-effect waves-cyan"><i className="mdi-action-dashboard"></i><span> Dashboard</span></a>
+          </li>
+          <li><a href=""><i className="material-icons">view_module</i></a></li>
+          <li><a href=""><i className="material-icons">refresh</i></a></li>
+          <li><a href="" onClick={this.toggleFullscreen}><i className="material-icons">settings_overscan</i></a></li>
+        </ul>
+      </section>
     );
   }
 }

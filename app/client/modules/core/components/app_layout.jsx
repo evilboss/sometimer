dbc42c7e016172cd3ca2e951301c4ee53b5813ec
aaron <aaronrandrup@gmail.com>
2016-05-13 10:42:04 +0800

    REM-20 REM-156 app login responsive layout

--- app/client/modules/core/components/app_layout.jsx
+++ app/client/modules/core/components/app_layout.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-const AppLayout = ({title, content = () =>null}) =>(
+const AppLayout = ({title, head = () => null, content =() =>null, footer =() =>null}) =>(
   <div>
     <Helmet title={title}
             meta={[
           {"name": "viewport", "content": "width=device-width, initial-scale=1"}
         ]}/>
-    {content()}
+    <section id="body">
+      <header>
+        {head()}
+      </header>
+      <main id="main">
+        {content()}
+      </main>
+      {footer()}
+    </section>
   </div>
 );
 
--- app/client/modules/users/components/Login.jsx
+++ app/client/modules/users/components/Login.jsx
@@ -3,7 +3,49 @@ import ReactDOM from 'react-dom';
 class Login extends React.Component {
   render() {
     return (
-      <section id="login" className="container">Login</section>
+      <section id="login">
+        <div className="container">
+          <div className="row">
+            <div className="col s12">
+              <div className="header-wrapper center-align">
+
+                <div className="logo">
+                  <img className="responsive-img" src="Assets/logo/ezyva-logo.png"/>
+                </div>
+
+                <div className="container">
+                  <div className="form row">
+
+                    <div className="blue ribbon">
+                      Login
+                    </div>
+
+                    <form className="col s12">
+                      <div className="row">
+                        <div className="input-field col s12">
+                          <i className="material-icons prefix">email</i>
+                          <input id="icon_prefix" type="email" className="validate"/>
+                          <label for="icon_prefix">Email</label>
+                        </div>
+                        <div className="input-field col s12">
+                          <i className="material-icons prefix">lock</i>
+                          <input id="icon_telephone" type="tel" className="validate"/>
+                          <label for="icon_telephone">Password</label>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+
+                <a className="waves-effect waves-light btn btn-large yellow darken-2">
+                  <i className="material-icons left">input</i>Sign In</a>
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </section>
     );
   }
 

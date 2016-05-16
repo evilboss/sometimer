489231baf494b1f44f41f96c97624f17061b3f7a
aaron <aaronrandrup@gmail.com>
2016-05-16 11:59:50 +0800

    REM-47 REM-158 partial status layout

--- app/client/modules/staff/components/in_out_board.jsx
+++ app/client/modules/staff/components/in_out_board.jsx
@@ -18,18 +18,25 @@ class InOutBoard extends React.Component {
                   </div>
                   <div className="right center-align">
                     <a href="/"><i className="material-icons">dashboard</i>
-                    <span>Dashboard</span>
+                      <span>Dashboard</span>
                     </a>
                   </div>
                 </div>
 
                 <div className="container">
                   <div className="row">
-                    <div className="col l6 m6 s12 center-align">
+                    <div className="col l4 m4 s12 center-align">
                       <img src="http://www.genengnews.com/app_themes/genconnect/images/default_profile.jpg" alt="dp"
                            className="display-photo circle responsive-img"/>
                     </div>
-                    <div className="col l6 m6 s12">This div is 6-columns wide</div>
+                    <div className="staff-details col l8 m8 s12">
+                      <h4>Staff Name</h4>
+                      <p><i className="material-icons left">work</i>Job Title</p>
+                      <b><i className="material-icons left">cached</i>Click to Change Status</b>
+                      <div>
+                        <span className="beacon"></span>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>

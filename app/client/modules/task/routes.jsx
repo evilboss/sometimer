990210cefdeb254e87844afb6aac5958f69654a1
Jr Reyes <jr.evilboss@gmail.com>
2016-05-17 16:36:42 +0800

    REM-63 Added new route for task

---
+++ app/client/modules/task/routes.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {mount} from 'react-mounter';
+import  {TaskList,TaskCreate} from './containers';
+import MainLayout from '/client/modules/core/components/main_layout.jsx';
+
+export default function (injectDeps, {FlowRouter}) {
+  const MainLayoutCtx = injectDeps(MainLayout);
+
+  FlowRouter.route('/task', {
+    name: 'task',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<TaskList/>)
+      });
+    }
+  });
+}

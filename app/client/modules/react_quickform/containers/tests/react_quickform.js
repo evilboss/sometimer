af390ef0e1ebe29b1d7139a696a743c94fe271a0
Jr Reyes <jr.evilboss@gmail.com>
2016-05-18 18:18:11 +0800

    REM-165 Task Create Added React Quickform containers

---
+++ app/client/modules/react_quickform/containers/tests/react_quickform.js
@@ -0,0 +1,20 @@
+const {describe, it} = global;
+import {expect} from 'chai';
+import {stub, spy} from 'sinon';
+import {composer} from '../react_quickform';
+
+describe('react_quickform.containers.react_quickform', () => {
+  describe('composer', () => {
+
+//    const Tracker = {nonreactive: cb => cb()};
+//    const getCollections = (post) => {
+//      const Collections = {
+//        Posts: {findOne: stub()}
+//      };
+//      Collections.Posts.findOne.returns(post);
+//      return Collections;
+//    };
+
+    it('should do something');
+  });
+});

759c99b03701540d14f1e6cb7debafc52ffbc37b
aaron <aaronrandrup@gmail.com>
2016-05-19 14:14:55 +0800

    REM-63 added task list header footer

--- app/client/modules/task/routes.jsx
+++ app/client/modules/task/routes.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {mount} from 'react-mounter';
-import  {TaskList,TaskCreate} from './containers';
+import  {TaskList, TaskCreate} from './containers';
+import {Header, Footer} from '../core/components';
 import MainLayout from '/client/modules/core/components/main_layout.jsx';
+
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
@@ -10,7 +12,9 @@ export default function (injectDeps, {FlowRouter}) {
     name: 'task',
     action() {
       mount(MainLayoutCtx, {
-        content: () => (<TaskList/>)
+        head: () => (<Header/>),
+        content: () => (<TaskList/>),
+        footer: () => (<Footer/>)
       });
     }
   });
@@ -19,6 +23,7 @@ export default function (injectDeps, {FlowRouter}) {
     action() {
       mount(MainLayoutCtx, {
         content: () => (<TaskCreate/>)
+
       });
     }
   });

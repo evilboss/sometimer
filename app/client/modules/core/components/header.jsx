7a74d12c36cdb5bc609116c63ea610046a40bd20
aaron <aaronrandrup@gmail.com>
2016-05-20 18:19:34 +0800

    REM-63 all subtask are done

--- app/client/modules/core/components/header.jsx
+++ app/client/modules/core/components/header.jsx
@@ -23,9 +23,7 @@ class Header extends React.Component {
 
               <ul className="right hide-on-med-and-down">
                 <li><a href=""><i className="mdi-social-notifications"></i></a></li>
-                <li><a href=""><i className="material-icons">view_module</i></a></li>
-                <li><a href=""><i className="material-icons">refresh</i></a></li>
-                <li><a href="" onClick={this.toggleFullscreen}><i className="material-icons">settings_overscan</i></a>
+                <li><a href=""><i className="material-icons">exit_to_app</i></a>
                 </li>
               </ul>
               <SideNav />

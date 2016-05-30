cff3734fc08049d66c5c4a0e8dfb4d26d3dee0c5
aaron <aaronrandrup@gmail.com>
2016-05-30 18:13:33 +0800

    added create team component

--- app/client/modules/team/routes.jsx
+++ app/client/modules/team/routes.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 import MainLayout from '/client/modules/core/components/main_layout.jsx';
+import CreateTeam from './components/create_team'
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
 
-  // FlowRouter.route('', {
-  //   name: '',
-  //   action() {
-  //     mount(MainLayoutCtx, {
-  //       content: () => (< />)
-  //     });
-  //   }
-  // });
+  FlowRouter.route('/team/create', {
+    name: 'team.create',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (< CreateTeam/>)
+      });
+    }
+  });
 }

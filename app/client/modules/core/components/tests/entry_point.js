b4a5baf71f9b4e1c243708a27d715313b3b41c47
aaron <aaronrandrup@gmail.com>
2016-05-30 17:52:03 +0800

    added files

---
+++ app/client/modules/core/components/tests/entry_point.js
@@ -0,0 +1,8 @@
+const {describe, it} = global;
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import EntryPoint from '../landing/entry_point';
+
+describe('core.components.entry_point', () => {
+  it('should do something');
+});

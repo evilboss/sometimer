cb720c59fabca93f3f5daf7e37e23c7b058c3412
Jr Reyes <jr.evilboss@gmail.com>
2016-06-08 16:29:18 +0800

    Added team publication to index

--- app/server/publications/index.js
+++ app/server/publications/index.js
@@ -2,10 +2,11 @@ import timesheet from './timesheet';
 import task from './task';
 import users from './users';
 import menu from './menu';
-
+import team from './team';
 export default function () {
   timesheet();
   task();
   users();
   menu();
+  team();
 }

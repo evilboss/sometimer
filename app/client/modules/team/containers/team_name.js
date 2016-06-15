dfdb8681daa7c97fb940004088a9dfc056d99c09
Jr Reyes <jr.evilboss@gmail.com>
2016-06-15 11:15:39 +0800

    Added test to team name view

---
+++ app/client/modules/team/containers/team_name.js
@@ -0,0 +1,18 @@
+import {useDeps, composeAll, composeWithTracker, compose} from 'mantra-core';
+
+import TeamName from '../components/create_team/forms/team_name.jsx';
+
+export const composer = ({context}, onData) => {
+  const {Meteor, Collections} = context();
+  Meteor.subscribe('team.list').ready()
+  const team = Collections.Team;
+  onData(null, {team});};
+
+export const depsMapper = (context, actions) => ({
+  context: () => context
+});
+
+export default composeAll(
+  composeWithTracker(composer),
+  useDeps(depsMapper)
+)(TeamName);

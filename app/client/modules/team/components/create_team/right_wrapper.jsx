0f4a960b7cc7cd2993e709e063272ec88ec623c8
aaron <aaronrandrup@gmail.com>
2016-06-17 15:17:50 +0800

    fixed create team layout added remotiv logo

--- app/client/modules/team/components/create_team/right_wrapper.jsx
+++ app/client/modules/team/components/create_team/right_wrapper.jsx
@@ -8,13 +8,18 @@ class RightWrapper extends React.Component {
   render() {
     return (
       <div className="right-wrapper col hide-on-med-and-down l7">
-
+        <div className="background-image"></div>
         <div className="header">
-        
+
         </div>
 
-        <div className="content container">
-
+        <div className="content container valign-wrapper">
+          <div className="translucent-background margin-auto center-align z-depth-4">
+            <div className="big-logo">
+              <img src="/Assets/teams/default/logo/remotiv_io_logo_style3.png"/></div>
+            <div className="remotiv-text"><h1>Remotiv<span>.io</span></h1><h6>
+              Business Beyond Boundaries</h6></div>
+          </div>
         </div>
 
         <div className="page-footer">

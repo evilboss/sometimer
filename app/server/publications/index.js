4f24b2ef4fed9c0181b90435a2b4b9c27456010a
Jr Reyes <jr.evilboss@gmail.com>
2016-06-27 16:08:56 +0800

    REM-36 Added new publication for timelogs

--- app/server/publications/index.js
+++ app/server/publications/index.js
@@ -3,10 +3,12 @@ import task from './task';
 import users from './users';
 import menu from './menu';
 import team from './team';
+import timelogs from './timelogs';
 export default function () {
   timesheet();
   task();
   users();
   menu();
   team();
+  timelogs();
 }

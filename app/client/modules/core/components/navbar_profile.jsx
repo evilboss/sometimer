b49433b86f9bd79987119749c6eb9dbe2379b36d
aaron <aaronrandrup@gmail.com>
2016-06-28 18:53:36 +0800

    added logout button

--- app/client/modules/core/components/navbar_profile.jsx
+++ app/client/modules/core/components/navbar_profile.jsx
@@ -4,7 +4,8 @@ class NavbarProfile extends React.Component {
   constructor(props) {
     super(props);
     this.logOut = ()=> {
-
+      Meteor.logout();
+      FlowRouter.go('/login');
     };
   }
 
@@ -15,7 +16,7 @@ class NavbarProfile extends React.Component {
         <li><a href="/profile">Profile
           <i className="material-icons right">account_box</i></a></li>
         <li className="divider"></li>
-        <li><a href="" onclick={this.logOut()}>Log out
+        <li><a href="" onClick={this.logOut()}>Log out
           <i className="material-icons right">exit_to_app</i></a></li>
       </ul>
     );

1c6d0b41b930acdf3b1e8de847a14c3bfd29b968
Jr Reyes <jr.evilboss@gmail.com>
2016-06-29 12:50:49 +0800

    REM-71 User Groups Added default routes to projects

--- app/client/modules/projects/routes.jsx
+++ app/client/modules/projects/routes.jsx
@@ -4,14 +4,26 @@ import {mount} from 'react-mounter';
 import MainLayout from '/client/modules/core/components/main_layout.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
-  const MainLayoutCtx = injectDeps(MainLayout);
 
-  // FlowRouter.route('', {
-  //   name: '',
-  //   action() {
-  //     mount(MainLayoutCtx, {
-  //       content: () => (< />)
-  //     });
-  //   }
-  // });
+  const MainLayoutCtx = injectDeps(MainLayout);
+  const projectRoutes = FlowRouter.group({
+    prefix: "/projects"
+  });
+  projectRoutes.route('/', {
+    action(){
+      console.log('Project root');
+    }
+  });
+  projectRoutes.route('/view', {
+    action(){
+      console.log('Project View');
+    }
+  });
+  projectRoutes.route('/:projectId', {
+    action(){
+      let projectId = FlowRouter.getParam('projectId');
+      console.log('Project ProjectID',projectId);
+    }
+  });
+
 }

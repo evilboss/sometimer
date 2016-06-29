844389011ad57bedc34bb37563e1ee1e56738f91
aaron <aaronrandrup@gmail.com>
2016-06-29 15:13:44 +0800

    added error localstate

--- app/client/modules/users/components/Login.jsx
+++ app/client/modules/users/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Form} from 'formsy-react';
+import Formsy from 'formsy-react';
 
 import MyInput from './input';
 /*TODO : @aaron fixed login layout and validation*/
@@ -7,8 +7,11 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
 
-    this.validSubmit = (data)=> {
+    this.onSubmit = (data)=> {
       this.props.submitAction(data.email, data.password);
+    };
+    this.validSubmit = (data)=> {
+
     };
     this.invalidSubmit = ()=> {
 
@@ -16,7 +19,7 @@ class Login extends React.Component {
   }
 
   render() {
-
+    const {err} = this.props;
     return (
       <section className="blue-theme" id="login">
 
@@ -34,14 +37,21 @@ class Login extends React.Component {
                     <div className="theme-color ribbon">
                       Login
                     </div>
-                    <Form onSubmit={this.validSubmit} onValid={this.validSubmit} onInvalid={this.invalidSubmit}
-                          className="login">
+
+                    {err ?
+                      <span className="error-container">
+                        <span className="error-text">
+                          {err}
+                        </span>
+                      </span> : null }
+                    <Formsy.Form onSubmit={this.onSubmit} onValid={this.validSubmit} onInvalid={this.invalidSubmit}
+                                 className="login">
                       <MyInput name="email" title="Email" validations="isEmail"
                                validationError="This is not a valid email" required/>
                       <MyInput name="password" title="Password" type="password" required/>
                       <button className="btn waves-effect waves-light theme-color" type="submit">Login <i
                         className="material-icons right">send</i></button>
-                    </Form>
+                    </Formsy.Form>
 
                   </div>
                 </div>

8057bdaa781def5a7afbea492b8e46cf35da0276
Jr Reyes <jr.evilboss@gmail.com>
2016-06-30 14:51:42 +0800

    Updated profile page

--- app/client/modules/users/components/Profile.jsx
+++ app/client/modules/users/components/Profile.jsx
@@ -54,29 +54,39 @@ export default class Profile extends React.Component {
   }
 
   render() {
-    const user = this.props.user;
+    const user = Meteor.user();
     return (
       <section className="profile">
         <h5 className="title">My Account</h5>
-        <div className="row">
-          <div className="col s12 m2 l2">
-            <img src="http://www.genengnews.com/app_themes/genconnect/images/default_profile.jpg" alt="dp"
-                 className="display-photo responsive-img"/>
-          </div>
-          <div className="col s12 m10 l10">
-            <h5>Account Information</h5>
-            <ul className="collection">
-              <li className="collection-item">User Name: <span>aaron@ezyva.com</span></li>
-              <li className="collection-item">First Name: <span>Aaron</span></li>
-              <li className="collection-item">Middle Name: <span>Manaloto</span></li>
-              <li className="collection-item">Last Name: <span>Randrup</span></li>
-              <li className="collection-item">Birthday: <span>06-17-1993</span></li>
-              <li className="collection-item">Email: <span>aaron@aaron.com</span></li>
-              <li className="collection-item">Mobile: <span>0999-999-19993</span></li>
-            </ul>
+        {(user) ?
+          <div className="row">
+            <div className="col s12 m2 l2">
+              <img
+                src={(user.profile.displayPhoto)?user.profile.displayPhoto:'http://www.genengnews.com/app_themes/genconnect/images/default_profile.jpg'}
+                alt="dp"
+                className="display-photo responsive-img"/>
+            </div>
+
+            <div className="col s12 m10 l10">
+              <h5>Account Information</h5>
+              <ul className="collection">
+                <li className="collection-item">Email: <span>{(user.emails[0]) ? user.emails[0].address : ''}</span>
+                </li>
+                <li className="collection-item">First Name:
+                  <span>{(user.profile.firstName) ? user.profile.firstName : ''}</span></li>
+                <li className="collection-item">Last Name:
+                  <span>{(user.profile.lastName) ? user.profile.lastName : ''}</span></li>
+                <li className="collection-item">Department:
+                  <span>{(user.profile.department) ? user.profile.department : ''}</span></li>
+                <li className="collection-item">Designation:
+                  <span>{(user.profile.jobTitle) ? user.profile.jobTitle : ''}</span></li>
+                <li className="collection-item">Status:
+                  <span>{(user.profile.staffType) ? user.profile.staffType : ''}</span></li>
+              </ul>
+            </div>
 
           </div>
-        </div>
+          : ''}
       </section>
     );
   }

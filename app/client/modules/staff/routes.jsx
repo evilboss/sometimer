9046a24663bafbdc5518212248c446d0643c0e80
aaron <aaronrandrup@gmail.com>
2016-07-13 12:11:48 +0800

    REM-272 done

--- app/client/modules/staff/routes.jsx
+++ app/client/modules/staff/routes.jsx
@@ -7,6 +7,8 @@ import Header from '../core/containers/header';
 import MainLayout from '/client/modules/core/components/main_layout.jsx';
 import {InOutBoard} from  './containers';
 import Profile from '../users/containers/Profile'
+import EditProfile from '../users/containers/profile_edit'
+
 /*TODO: add dashboardRoutes */
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
@@ -31,6 +33,14 @@ export default function (injectDeps, {FlowRouter}) {
     name: 'users.profile', action() {
       mount(MainLayoutCtx, {
         head: () => (<Header />), content: () => (<Profile />), footer: () => (<Footer />)
+      });
+    }
+  });
+
+  dashboardRoutes.route('/profile/edit', {
+    name: 'users.profile.edit', action() {
+      mount(MainLayoutCtx, {
+        head: () => (<Header />), content: () => (<EditProfile />), footer: () => (<Footer />)
       });
     }
   });

5e613a478c7aa596a0979071b16a8f6b6d543b29
aaron <aaronrandrup@gmail.com>
2016-07-13 12:13:37 +0800

    removed unused functions

--- app/client/modules/users/actions/users.js
+++ app/client/modules/users/actions/users.js
@@ -1,12 +1,5 @@
 export default {
 
-  update({Meteor, LocalState}, field) {
-    Meteor.call('user.update', field, (err) => {
-      if (err) {
-        notify.show(err.message, 'error');
-      }
-    });
-  },
 
   login({Meteor, LocalState, FlowRouter}, email, password) {
 
@@ -24,82 +17,5 @@ export default {
       FlowRouter.go('/dashboard/inOutBoard');
     });
 
-  },
-  recover_password({Meteor, LocalState}, email) {
-    Accounts.forgotPassword({email: email}, (err) => {
-      if (err) {
-        notify.show(err.message, 'error');
-      } else {
-        notify.show("Email sent. Please check your mail account.", 'success');
-      }
-    });
-  },
-
-  reset_password({Meteor, LocalState}, token, password, repeatPassword) {
-    if (password != repeatPassword) {
-      notify.show("Passwords don't match.", 'error');
-    } else {
-      Accounts.resetPassword(token, password, (err) => {
-        if (err) {
-          notify.show(err.message, 'error');
-        } else {
-          notify.show("New password has been saved.", 'success');
-          FlowRouter.go("/login");
-        }
-      });
-    }
-  },
-
-  change_password({Meteor, LocalState}, oldPassword, newPassword) {
-    Accounts.changePassword(oldPassword, newPassword, (err) => {
-      if (err) {
-        notify.show(err.message, 'error');
-      } else {
-        notify.show("New password has been saved.", 'success');
-      }
-    });
-  },
-
-  change_username({Meteor, LocalState}, username) {
-    Meteor.call('user.update', {username: username}, (err) => {
-      if (err) {
-        notify.show(err.message, 'error');
-      } else {
-        notify.show("New username has been saved.", 'success');
-      }
-    });
-  },
-
-  change_email({Meteor, LocalState}, email) {
-    Meteor.call('user.update', {email: email}, (err) => {
-      if (err) {
-        notify.show(err.message, 'error');
-      } else {
-        notify.show("New email has been saved.", 'success');
-        FlowRouter.go("/email-verification");
-      }
-    });
-  },
-
-  verify_email({Meteor, LocalState}, token) {
-    var result = true;
-    Accounts.verifyEmail(token, (err) => {
-      if (err) {
-        notify.show(err.message, 'error');
-      } else {
-        result = true;
-      }
-    });
-    return result;
-  },
-
-  send_verification_email({Meteor, LocalState}) {
-    Meteor.call("user.send_verification_email", (err) => {
-      if (err) {
-        notify.show(err.message, 'error');
-      } else {
-        FlowRouter.go("/email-verification");
-      }
-    });
-  },
+  }
 };

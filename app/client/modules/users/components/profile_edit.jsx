e2949e0b65f11f8fc93a426b9b18dc75e12efc1e
aaron <aaronrandrup@gmail.com>
2016-07-18 18:20:52 +0800

    REM-294 done REM-295 done

--- app/client/modules/users/components/profile_edit.jsx
+++ app/client/modules/users/components/profile_edit.jsx
@@ -1,32 +1,31 @@
 import React from 'react';
 import Quickform from '/client/modules/reactUtils/components/quickform';
+import Dropzone from 'react-dropzone';
 import Formsy from 'formsy-react';
 import MyInput from './input';
-import Dropzone from 'react-dropzone';
 
 class ProfileEdit extends React.Component {
   constructor(props) {
     super(props);
-    this.componentDidUpdate = ()=> {
-      this.onDrop = (files)=> {
-        console.log('Received files: ', files);
-        _.each(files, function (file) {
-          file.owner = Meteor.userId(); //before upload also save the owner of that file
-          Images.insert(file, function (err, fileObj) {
-            console.log(fileObj);
-            if (err) {
-              console.log(err); //in case there is an error, log it to the console
-            } else {
-              console.log(file);
-              console.log("Success -> File Id: ->", fileObj._id);
-              let imagePath = 'images-' + fileObj._id + '-' + file.name;
 
-              console.log(Meteor.call('user.update-photo', Meteor.userId(), imagePath));
+    this.onDrop = (files)=> {
+      console.log('Received files: ', files);
+      _.each(files, function (file) {
+        file.owner = Meteor.userId(); //before upload also save the owner of that file
+        Images.insert(file, function (err, fileObj) {
+          console.log(fileObj);
+          if (err) {
+            console.log(err); //in case there is an error, log it to the console
+          } else {
+            console.log(file);
+            console.log("Success -> File Id: ->", fileObj._id);
+            let imagePath = 'images-' + fileObj._id + '-' + file.name;
 
-            }
-          });
+            console.log(Meteor.call('user.update-photo', Meteor.userId(), imagePath));
+
+          }
         });
-      };
+      });
     };
     this.onSubmit = (data)=> {
       this.props.submitAction(data.oldPassword, data.newPassword);
@@ -48,29 +47,37 @@ class ProfileEdit extends React.Component {
                 alt="dp"
                 className="display-photo responsive-img center-block"/>
 
-              <Dropzone onDrop={this.onDrop}>
-                <div>Try dropping some files here, or click to select files to upload.</div>
+              <Dropzone className="dropzone-wrapper" onDrop={this.onDrop}>
+                <div className="dropzone-container">Drop File here or click to upload.
+                </div>
               </Dropzone>
             </div>
-            <div className="col s12 m10 l10 no-horizontal-margin row z-depth-1-half card-top-border">
-
-              <Formsy.Form onSubmit={this.onSubmit} onValid={this.validSubmit} onInvalid={this.invalidSubmit}
-                           className="login">
-                <MyInput name="oldPassword" title="oldPassword" type="password"/>
-                <MyInput name="newPassword" title="newPassword" type="password"/>
-                <button className="btn waves-effect waves-light theme-color" type="submit">Change Password
-                  <i className="material-icons right">send</i></button>
-              </Formsy.Form>
-
-
-              {<Quickform
-                buttonText="Update Profile"
-                field={this.props.users}
-                doc={this.props.user}
-                ommited="emails,profile.status,services,createdAt,roles,profile.displayPhoto"
-                operation="update"
-                name="updateProfileForm"
-              />}
+            <div className="col s12 m10 l10">
+              <h5 class="title">Account Information</h5>
+              <div className="no-horizontal-margin row z-depth-1-half card-top-border">
+                {<Quickform
+                  buttonText="Update Profile"
+                  field={this.props.users}
+                  doc={this.props.user}
+                  ommited="emails,profile.status,services,createdAt,roles,profile.displayPhoto"
+                  operation="update"
+                  name="updateProfileForm"
+                />}
+              </div>
+              <h5 class="title">Account Password</h5>
+              <div className="no-horizontal-margin row z-depth-1-half card-top-border">
+                <Formsy.Form onSubmit={this.onSubmit} onValid={this.validSubmit} onInvalid={this.invalidSubmit}
+                             className="login">
+                  <MyInput name="oldPassword" title="oldPassword" type="password"/>
+                  <MyInput name="newPassword" title="newPassword" type="password"/>
+                  <div className="row">
+                    <div className="col s12">
+                      <button className="btn waves-effect waves-light theme-color" type="submit">Change Password
+                      </button>
+                    </div>
+                  </div>
+                </Formsy.Form>
+              </div>
             </div>
           </div>
           : ''}

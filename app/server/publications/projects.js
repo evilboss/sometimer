f9db1e29bece171f19d06f34b44ea880788adef3
Jr Reyes <jr.evilboss@gmail.com>
2016-08-04 17:04:48 +0800

    Updated project publication

--- app/server/publications/projects.js
+++ app/server/publications/projects.js
@@ -4,6 +4,25 @@ import {check} from 'meteor/check';
 
 export default function () {
   Meteor.publish('project-list', function () {
-    return Projects.find();
+    const userId = '8bDnNNvhPzQw3BHxj';
+    console.log('projectList',Projects.find().fetch());
+    const selector = {
+      members: {$all: [this.userId]}
+    };
+    const options = {};
+    console.log(Projects.find(selector, options).fetch());
+    return Projects.find(selector, options);
+  });
+
+  Meteor.publish('project.single', function (projectId) {
+    check(projectId, String);
+    const selector = {_id: projectId};
+    return Projects.find(selector);
+  });
+
+  Meteor.publish('project.task', function (postId) {
+    check(postId, String);
+    const selector = {projectId};
+    return Task.find(selector);
   });
 }

67ca79444d6c0bae1432d740eaebed0ecbd9802e
aaron <aaronrandrup@gmail.com>
2016-08-04 15:47:38 +0800

    added files

---
+++ app/client/modules/projects/components/tests/project_view.js
@@ -0,0 +1,8 @@
+const {describe, it} = global;
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import ProjectView from '../project_view';
+
+describe('projects.components.project_view', () => {
+  it('should do something');
+});

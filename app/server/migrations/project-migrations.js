293fac49e67b6cc84b57937f9e962a491eb6c555
Jr Reyes <jr.evilboss@gmail.com>
2016-08-04 17:06:04 +0800

    Added new migration project-migrations

---
+++ app/server/migrations/project-migrations.js
@@ -0,0 +1,27 @@
+/**
+ * Created by jr on 8/4/16.
+ */
+import {Projects} from '/lib/collections';
+const projectnames = ['Project 1', 'project 2', 'test'];
+export function loadProjects() {
+  let userList = ['jr@ezyva.com', 'admin@admin.com', 'aaron.randrup@ezyva.com'];
+  let members = [];
+  _.each(userList, function (userEmail) {
+    let member = Meteor.users.findOne({"emails.address": userEmail});
+    console.log(member);
+    members.push(member._id);
+  });
+  console.log(members);
+  console.log('Adding projects');
+  _.each(projectnames, function (project) {
+    Projects.insert({name: project, createdAt: Date.now(), members: members});
+  });
+  console.log('displaying projects', Projects.find().fetch());
+}
+export function removeProjects() {
+  let projectnames = ['Project 1', 'project 2', 'test'];
+  _.each(projectnames, function (project) {
+    Projects.remove({name: project});
+  });
+  console.log('displaying projects', Projects.find().fetch());
+}

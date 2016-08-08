e1ff17a2359994bf71baa2450cd9b79780bf5962
Jr Reyes <jr.evilboss@gmail.com>
2016-08-08 16:39:19 +0800

    Added new component and container timedata

---
+++ app/client/modules/timesheet/components/tests/timedata.js
@@ -0,0 +1,8 @@
+const {describe, it} = global;
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import Timedata from '../timedata';
+
+describe('staff.components.timedata', () => {
+  it('should do something');
+});

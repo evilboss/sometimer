2d91ef62678a3c67edf24c5aa16b6ff0b8331519
Jr Reyes <jr.evilboss@gmail.com>
2016-08-08 16:37:15 +0800

    added new component timedata

---
+++ app/client/modules/timesheet/components/timedata.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Timedata extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <td>
+        Timedata
+      </td>
+    );
+  }
+}
+
+export default Timedata;

cc7554525b8d7d83243e7b9b9aaf2d30ec43bcad
Jr Reyes <jr.evilboss@gmail.com>
2016-08-08 17:56:37 +0800

    Added color in active date and weekend

--- app/client/modules/timesheet/components/timedata.jsx
+++ app/client/modules/timesheet/components/timedata.jsx
@@ -6,12 +6,20 @@ class Timedata extends React.Component {
   }
 
   isToday(date) {
-   return ((moment(date).isSame(moment(), 'day')))?'table-row-active':'';
+    return ((moment(date).isSame(moment(), 'day'))) ? 'table-row-active' : '';
   }
+
+  isWeekend(date) {
+    return ((moment(date).isoWeekday() == 6) || (moment(date).isoWeekday() == 7))?'table-row-weekend':'';
+  }
+  getRowClass(date){
+    return this.isWeekend(date)+' '+this.isToday(date);
+  }
+
   render() {
     const timelog = this.props.timelog;
     return (
-      <tr key={this.props.keyIndex} className={this.isToday(this.props.date)}>
+      <tr key={this.props.keyIndex} className={this.getRowClass(this.props.date)}>
         <td>{this.props.date.toDateString()}</td>
         <td>
           {(timelog) ? (timelog.timeIn) ? moment(timelog.timeIn).format('hh:mm:ss') : '00:00:00' : '--:--:--'}
--- app/client/modules/timesheet/containers/timedata.js
+++ app/client/modules/timesheet/containers/timedata.js
@@ -4,7 +4,6 @@ import moment from 'moment';
 
 export const composer = ({context, date, userId,keyIndex}, onData) => {
   const {Meteor, Collections} = context();
-  console.log(date,userId,keyIndex);
   if (Meteor.subscribe('timelogs.by.date', moment(date).format('DD:MM:YY'), userId).ready) {
     const selector = {date: moment(date).format('DD:MM:YY'), userId: userId};
     const timelog = Collections.Timelogs.findOne(selector);

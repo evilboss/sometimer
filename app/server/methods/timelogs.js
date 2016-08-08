3f53820e6cac291aeeabd07d652d145a8e9f6083
Jr Reyes <jr.evilboss@gmail.com>
2016-08-08 13:06:05 +0800

    Added timelog server method

--- app/server/methods/timelogs.js
+++ app/server/methods/timelogs.js
@@ -1,14 +1,20 @@
 import {Timelogs} from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
-import {dtr} from './dtr/dtr';
+import {timelogs} from './timelogs/timelogs';
 export default function () {
   Meteor.methods({
-    'timelogs.change-status'(){
-      let currentUser = Meteor.users.findOne({_id: Meteor.userId()});
-      let status = currentUser.profile.status;
-      dtr.changeStatus(status);
-
+    'timelogs.startShift'(){
+      timelogs.startShift();
+    },
+    'timelogs.startBreak'(){
+      timelogs.startBreak();
+    },
+    'timelogs.endBreak'(){
+      timelogs.endBreak();
+    },
+    'timelogs.endShift'(){
+      timelogs.endShift();
     }
 
   });

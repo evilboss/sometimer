e9dac43d284a56b47ca4fd8184ba974c07b77946
Jr Reyes <jr.evilboss@gmail.com>
2016-08-18 14:48:22 +0800

    Edited stafflist to work with new sendinvite button

--- app/client/modules/client-module/components/staff_list.jsx
+++ app/client/modules/client-module/components/staff_list.jsx
@@ -13,7 +13,7 @@ class StaffList extends React.Component {
           <h5>Staff List</h5>
           <div className="collection">
             {this.props.staffList.map((staff, index) => (
-              <a className="collection-item avatar" key={index} href={`/dashboard/staff/${staff._id}`>
+              <a className="collection-item avatar" key={index} href={`/dashboard/staff/${staff._id}`}>
                 <img src={"/uploads/" + staff.profile.displayPhoto} alt="" className="circle"/>
                 <span className="title">{staff.profile.firstName} {staff.profile.lastName}</span>
                 <p>{staff.profile.jobTitle}<br/>

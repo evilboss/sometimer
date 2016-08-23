98d2a4c1195afb63d2ca5909a62febd11e5dd44d
aaron <aaronrandrup@gmail.com>
2016-08-23 18:16:23 +0800

    partial horizontal menu layout

--- app/client/modules/dashboard/components/dashboard_header.jsx
+++ app/client/modules/dashboard/components/dashboard_header.jsx
@@ -20,7 +20,7 @@ class DashboardHeader extends React.Component {
     return (
       <section id="header">
         <nav>
-          <div className="nav-wrapper theme-color">
+          <div className="nav-wrapper">
             <a href="/dashboard" className="brand-logo">
               <img className="inline" src="/Assets/teams/default/logo/remotiv_io_logo_style3.png"/>
               <div className="remotiv-text inline"><h5>Remotiv<span>.io</span>

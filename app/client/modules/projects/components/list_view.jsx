c446784e03be1af9ab25284c12583b8cf9201206
aaron <aaronrandrup@gmail.com>
2016-08-24 18:52:35 +0800

    listview and tile view new layout done

--- app/client/modules/projects/components/list_view.jsx
+++ app/client/modules/projects/components/list_view.jsx
@@ -8,21 +8,34 @@ class ListView extends React.Component {
   render() {
     return (
       <section className="project-list">
-        <div><h5>ProjectList <span>
-          <a href="/projects/new" className="btn-floating waves-effect waves-light theme-color">
-            <i className="material-icons">add</i></a></span></h5>
+        <div className="page-title">
+          <h5 className="inline">ProjectList</h5>
+
+          <div className="project-view inline">
+            <a href="/projects/tileview"><i className="material-icons ">view_module</i></a>
+            <i className="material-icons active">list</i>
+          </div>
         </div>
-        <div className="project-view">
-          <a href="/projects/tileview"><i className="material-icons">view_module</i></a>
-          <i className="material-icons active">view_list</i>
-        </div>
-        <div className="collection">
-          {this.props.projects.map(project => (
-            <a href={`/project/${project._id}`} key={project._id} className="collection-item">
-              
-              <b>{project.name}</b>
-            </a>
-          ))}
+
+
+        <div className="row">
+          <div className="col s2 center-align">
+            <div className="btn-add">
+              <a href="/projects/new" className="waves-effect waves-light theme-color">
+                <i className="material-icons">add</i></a>
+              <h6>New Project</h6>
+            </div>
+          </div>
+          <div className="col s10">
+            <div className="collection">
+              {this.props.projects.map(project => (
+                <a href={`/project/${project._id}`} key={project._id} className="collection-item">
+
+                  <b>{project.name}</b>
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
       </section>
     );

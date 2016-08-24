03d673a638bb3a29bcb36fd550dd648a9925473e
Jr Reyes <jr.evilboss@gmail.com>
2016-08-24 16:15:32 +0800

    Fixed Profile routes

--- app/client/modules/staff/routes.jsx
+++ app/client/modules/staff/routes.jsx
@@ -10,12 +10,10 @@ import {TimeTracker} from  './containers';
 import Profile from '../users/containers/Profile';
 import {dashboardRoutes} from '/client/modules/dashboard/routes'
 import EditProfile from '../users/containers/profile_edit'
-
 /*TODO: add dashboardRoutes */
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
   const PunchCardLayoutCtx = injectDeps(PunchcardLayout);
-
   dashboardRoutes.route('/timetracker', {
     name: 'users.timetracker',
     action() {

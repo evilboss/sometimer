6da290635fa568c0a6b8d0c3f22e28cb90640bb6
Jr Reyes <jr.evilboss@gmail.com>
2016-09-01 18:51:13 +0800

    Fixed some stuff on timelogs

--- app/client/modules/staff/components/staff_list.jsx
+++ app/client/modules/staff/components/staff_list.jsx
@@ -3,9 +3,15 @@ import moment from 'moment';
 import SendInvitationModal from '/client/modules/invitations/components/send_invitation_modal';
 import StaffDetails from '../containers/staff_details';
 import PageTitle from '/client/modules/core/components/page_title';
+import DatePicker from '/client/modules/timesheet/components/datepicker';
 class StaffList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dates: [],
+      from: moment().format('LL'),
+      to: moment().format('LL'),
+    };
   }
 
   componentDidMount() {
@@ -14,8 +20,36 @@ class StaffList extends React.Component {
     });
   }
 
+  getWeek() {
+    var curr = new Date;
+    var firstday = new Date(curr.setDate(curr.getDate() - curr.getDay()));
+    var lastday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 6));
+    console.log(firstday, lastday);
+  }
+
   changeView(e) {
     console.log(e);
+  }
+
+  getDates(from = null, to = null) {
+    const reactState = this;
+    Meteor.call('timesheet_dates.getCutOffDates', from, to, function (err, res, callback) {
+      if (err) {
+        sweatAlert(
+          'Ooops',
+          'Something went wrong!',
+          '' + JSON.stringify(err, null, 2)
+        );
+
+      } else {
+        reactState.setState({dates: res});
+      }
+
+    });
+  }
+
+  changeDate(from, to) {
+    this.getDates(from, to);
   }
 
   render() {
@@ -25,17 +59,25 @@ class StaffList extends React.Component {
           <PageTitle title="My Team"/>
           <div className="row">
             <div className="col s12">
-              <div id="today" className="col s12">Test 1</div>
-              <div id="week" className="col s12">Test 2</div>
-              <div id="month" className="col s12">Test 3</div>
-              <div id="custom" className="col s12">Test 4</div>
+              <div id="today" className="col s12">
+                Today is {moment().format('LL')}
+              </div>
+              <div id="week" className="col s12">
+                Period from {this.state.from} to {this.state.to}
+              </div>
+              <div id="month" className="col s12">
+                Period from {this.state.from} to {this.state.to}
+              </div>
+              <div id="custom" className="col s12">
+                <DatePicker changeDate={this.changeDate.bind(this)}/>
+              </div>
             </div>
           </div>
           <div className="tabs-wrapper">
             <ul className="tabs">
               <li className="tab col s3"><a onClick={this.changeView.bind(this)} className="active"
                                             href="#today">Today</a></li>
-              <li className="tab col s3"><a onClick={this.changeView.bind(this)} href="#week">This Week</a></li>
+              <li className="tab col s3"><a onClick={this.getWeek.bind(this)} href="#week">This Week</a></li>
               <li className="tab col s3"><a onClick={this.changeView.bind(this)} href="#month">This Month</a></li>
               <li className="tab col s3"><a onClick={this.changeView.bind(this)} href="#custom">Custom Date</a></li>
             </ul>
--- app/client/modules/staff/containers/staff_details.js
+++ app/client/modules/staff/containers/staff_details.js
@@ -1,9 +1,12 @@
 import {useDeps, composeAll, composeWithTracker, compose} from 'mantra-core';
+import moment from 'moment';
 
 import StaffDetails from '../components/staff_details.jsx';
 
-export const composer = ({context, staff, index}, onData) => {
+export const composer = ({context, staff, index, startDate, endDate}, onData) => {
   const {Meteor, Collections} = context();
+  //const subscriptionReady = [Meteor.subscribe('timelogs.by.date', moment(new Date()).format('DD:MM:YY'), staff._id).ready];
+  //console.log(Collections.Timelogs.find().fetch());
   onData(null, {staff, index});
 };
 

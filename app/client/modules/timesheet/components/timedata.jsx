6ccdfe2ce1f64beb914fb5712d498ef69713eff6
aaron <aaronrandrup@gmail.com>
2016-09-13 16:59:38 +0800

    timesheet edit time

--- app/client/modules/timesheet/components/timedata.jsx
+++ app/client/modules/timesheet/components/timedata.jsx
@@ -20,6 +20,34 @@ class Timedata extends React.Component {
     return this.isWeekend(date) + ' ' + this.isToday(date);
   }
 
+  endEdit(e) {
+    let defaultText = this.props.timelog.timeIn;
+    var input = $(e.target),
+      label = input && input.prev();
+
+    label.text(input.val() === '' ? defaultText : input.val());
+    input.hide();
+    label.show();
+  }
+
+  componentDidMount() {
+    $('.clickedit').hide()
+      .focusout(this.endEdit)
+      .keyup(function (e) {
+        if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+          this.endEdit(e);
+          return false;
+        } else {
+          return true;
+        }
+      })
+      .prev().click(function () {
+      $(this).hide();
+      $(this).next().show().focus();
+    });
+  }
+
+
   render() {
     const timelog = this.props.timelog;
     const userRole = this.props.activeRole;
@@ -28,13 +56,18 @@ class Timedata extends React.Component {
       <tr key={this.props.keyIndex} className={this.getRowClass(this.props.date)}>
         <td>{this.props.date.toDateString()}</td>
         <td className="time-in">
+          <span>
           {(timelog) ? (timelog.timeIn) ? (moment(timelog.timeIn).tz('Asia/Manila').format(format)) : '' : ''}
+            </span>
+          <div className="clickedit">
+            <input type="text"/><i className="material-icons">border_color</i>
+          </div>
           {(userRole == 'manager' || userRole == 'admin') ?
             (timelog) ?
               (timelog._id) ?
                 <span>
-                  <div data-target={timelog._id} className="chip z-depth-1 modal-trigger" data-toggle="modal">
-                    Edit
+                  <div data-target={timelog._id} className="edit modal-trigger" data-toggle="modal">
+                    <i className="material-icons">border_color</i>
                   </div>
                   <EditHoursRendered target={timelog._id}/>
                 </span>
@@ -47,13 +80,18 @@ class Timedata extends React.Component {
             <a href={`/dashboard/timesheet/breaks/${timelog._id}`}>{timelog.totalBreak}</a> : '' : ''}
         </td>
         <td className="time-out">
+          <span>
           {(timelog) ? (timelog.timeOut) ? (moment(timelog.timeOut).tz('Asia/Manila').format(format)) : '' : ''}
+          </span>
+          <div className="clickedit">
+            <input type="text"/><i className="material-icons">border_color</i>
+          </div>
           {(userRole == 'manager' || userRole == 'admin') ?
             (timelog) ?
               (timelog._id) ?
                 <span>
-                  <div data-target={timelog._id} className="chip z-depth-1 modal-trigger" data-toggle="modal">
-                    Edit
+                  <div data-target={timelog._id} className="edit modal-trigger" data-toggle="modal">
+                     <i className="material-icons">border_color</i>
                   </div>
                   <EditHoursRendered target={timelog._id}/>
                 </span>

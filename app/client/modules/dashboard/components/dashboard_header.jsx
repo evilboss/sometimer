28b36a97275ee0cac04ccecc69ec7c585ba98ba2
Jr Reyes <jr.evilboss@gmail.com>
2016-09-15 10:29:39 +0800

    refactored header to work with dashboard and normal view

--- app/client/modules/dashboard/components/dashboard_header.jsx
+++ app/client/modules/dashboard/components/dashboard_header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import NavbarProfile from '../../core/components/navbar_profile';
+import HeaderMenu from '/client/modules/core/components/header_menu';
+
 class DashboardHeader extends React.Component {
   constructor(props) {
     super(props);
@@ -19,45 +21,11 @@ class DashboardHeader extends React.Component {
   render() {
     const currentUser = this.props.currentUser;
     return (
-      <section id="header">
-        <nav>
-          {(currentUser) ?
-            <div className="nav-wrapper">
-              <a href="/dashboard" className="brand-logo">
-                <div className="remotiv-text inline">Company Logo</div>
-              </a>
-              <ul className="right">
-                <li>
-                  <a href="#" data-activates="nav-mobile" className="button-collapse btn-menu"><i
-                    className="material-icons">menu</i></a>
-                </li>
-              </ul>
+      <div id="dashboardHeader">
+        <HeaderMenu currentUser={currentUser}/>
+      </div>
 
-              <ul className="right hide-on-med-and-down">
-                <li>Welcome back, {(currentUser.profile.firstName) ? currentUser.profile.firstName : ''}!
-                  <span></span>
-                </li>
-                <li><a href="">
-                  <i className="mdi-social-notifications"></i>
-                </a></li>
-                <li>
-                  <a href='#' className="account-menu" data-activates='account-menu'>
-                    <img className="inline"
-                         src={(currentUser.profile.displayPhoto) ? `/uploads/${currentUser.profile.displayPhoto}` : '/uploads/defaults/default-img.png'}/>
-                  </a>
-                  <NavbarProfile />
-                </li>
-                <li>
-                  <a href="">
-                    <i className="material-icons">settings</i>
-                  </a>
-                </li>
-                <li><img className="inline" src="/Assets/teams/default/logo/remotiv_io_logo_style3.png"/></li>
-              </ul>
 
-            </div> : ''}
-        </nav>
-      </section>
     );
   }
 }

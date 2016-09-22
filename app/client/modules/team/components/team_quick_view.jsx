c45aef999c329e0e4df8b63bf3e33f02d757798d
aaron <aaronrandrup@gmail.com>
2016-09-22 19:02:00 +0800

    fixed staff timesheet removed console logs in publications

---
+++ app/client/modules/team/components/team_quick_view.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class TeamQuickView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="collection">
+          <a href='' className="collection-item">
+            <b>ss</b>
+          </a>
+      </div>
+    );
+  }
+}
+
+export default TeamQuickView;
---
+++ app/client/modules/team/components/tests/team_quick_view.js
@@ -0,0 +1,8 @@
+const {describe, it} = global;
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import TeamQuickView from '../team_quick_view';
+
+describe('team.components.team_quick_view', () => {
+  it('should do something');
+});

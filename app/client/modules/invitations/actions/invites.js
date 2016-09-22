1d6558d6dd92e99a9081cd0e0f9b001462e9849b
Jr Reyes <jr.evilboss@gmail.com>
2016-09-22 16:20:42 +0800

    Added updates to invite actions

--- app/client/modules/invitations/actions/invites.js
+++ app/client/modules/invitations/actions/invites.js
@@ -1,5 +1,24 @@
 export default {
-  create({Meteor, LocalState}, projectId, text) {
-    
+  create({Meteor, LocalState}, invite) {
+    if (!invite.email) {
+      LocalState.set('CREATE_INVITE_ERROR', 'Email is a required field.');
+      return;
+    }
+    if (!invite.status) {
+      LocalState.set('CREATE_INVITE_ERROR', 'Status is a required field');
+      return;
+    }
+    if (!invite.status) {
+      LocalState.set('CREATE_INVITE_ERROR', 'Status is a required field');
+      return;
+    }
+    console.log(invite);
+
+    /*Meteor.call('invitations.send', invite, (error, response) => {
+      (error) ? alert(error.reason) : alert('Invitation Sent!');
+    });*/
+  },
+  clearErrors({LocalState}) {
+    return LocalState.set('CREATE_INVITE_ERROR', null);
   }
 }

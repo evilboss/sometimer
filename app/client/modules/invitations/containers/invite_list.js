1106a09c2c89b34e232309e218c0bed800fd1518
Jr Reyes <jr.evilboss@gmail.com>
2016-09-22 16:21:11 +0800

    Added updates to invite containers

--- app/client/modules/invitations/containers/invite_list.js
+++ app/client/modules/invitations/containers/invite_list.js
@@ -2,13 +2,16 @@ import {useDeps, composeAll, composeWithTracker, compose} from 'mantra-core';
 
 import InviteList from '../components/invite_list.jsx';
 
-export const composer = ({context}, onData) => {
-  const {Meteor, Collections} = context();
-
+export const composer = ({context, clearErrors}, onData) => {
+  const {Meteor, Collections, LocalState} = context();
+  const error = LocalState.get('CREATE_INVITE_ERROR');
   onData(null, {});
+
 };
 
 export const depsMapper = (context, actions) => ({
+  create: actions.invites.create,
+  clearErrors: actions.invites.clearErrors,
   context: () => context
 });
 
--- app/client/modules/invitations/containers/send_invitation_modal.js
+++ app/client/modules/invitations/containers/send_invitation_modal.js
@@ -1,11 +1,15 @@
 import {useDeps, composeAll, composeWithTracker, compose} from 'mantra-core';
 import SendInvitationModal from '../components/send_invitation_modal.jsx';
 export const composer = ({context}, onData) => {
-  const {Meteor, Collections} = context();
-  onData(null, {});
+  const {Meteor, Collections, LocalState} = context();
+  const error = LocalState.get('CREATE_INVITE_ERROR');
+  onData(null, {error});
+  return clearErrors;
 };
 
 export const depsMapper = (context, actions) => ({
+  create: actions.invites.create,
+  clearErrors: actions.invites.clearErrors,
   context: () => context
 });
 

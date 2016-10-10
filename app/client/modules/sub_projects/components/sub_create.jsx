5ba8d4f962b68c046ee37108d4f2248340ed918a
Jr Reyes <jr.evilboss@gmail.com>
2016-10-10 20:34:03 +0800

    Add prop settings to subcreate component

--- app/client/modules/sub_projects/components/sub_create.jsx
+++ app/client/modules/sub_projects/components/sub_create.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StaffMultiSelect from '/client/modules/staff/containers/staff_multi_select';
+import {control} from '/lib/access-control/control';
 class SubCreate extends React.Component {
   constructor(props) {
     super(props);
@@ -13,30 +14,41 @@ class SubCreate extends React.Component {
   }
 
   render() {
-    const {error} = this.props;
+    const {error, userPermissions} = this.props;
     return (
-      <div class="sub-create">
+      <div>
+        {
+          (userPermissions) ? control.isPermitted('createSubProject', userPermissions) ?
+            <div class="sub-create">
 
-        <h4>Create Sub Project</h4>
-        {error ? this._renderError(error) : null}
+              <h4>Create Sub Project</h4>
+              {error ? this._renderError(error) : null}
 
-        <div className="input-field col s12">
-          <input id="name" ref="name" type="text" className="validate"/>
-          <label htmlFor="name">Subproject Title</label>
-        </div>
+              <div>
+                <div className="input-field col s12">
+                  <input id="name" ref="name" type="text" className="validate"/>
+                  <label htmlFor="name">Subproject Title</label>
+                </div>
 
-        <div className="input-field col s12">
-          <textarea ref='details' placeholder='Details your comment here.' className="materialize-textarea">
-        </textarea>
-          <label htmlFor="textarea1">Details</label>
-        </div>
+                <div className="input-field col s12">
+            < textarea ref='details' placeholder='Details your comment here.' className="materialize-textarea">
+          </textarea>
+                  <label htmlFor="textarea1">Details</label>
+                </div>
 
-        <StaffMultiSelect getData={this.getData.bind(this)}/>
-        <button className="btn waves-effect waves-light theme-color" onClick={this._create.bind(this)}>Add
-          Sub-Project
-        </button>
+                <StaffMultiSelect getData={this.getData.bind(this)}/>
+                <button className="btn waves-effect waves-light theme-color" onClick={this._create.bind(this)}>Add
+                  Sub-Project
+                </button>
+              </div>
+
+            </div>
+            : '' : ''
+        }
       </div>
-    );
+
+    )
+      ;
   }
 
   _create() {

b7b4a5a4aa7e3848286496f777e3b226c2e17a17
Jr Reyes <jr.evilboss@gmail.com>
2016-10-10 20:32:46 +0800

    Add Team Containers

---
+++ app/client/modules/team/containers/team.js
@@ -0,0 +1,22 @@
+import {useDeps, composeAll, composeWithTracker, compose} from 'mantra-core';
+
+import Team from '../components/team.jsx';
+
+export const composer = ({context}, onData) => {
+  const {Meteor, Collections} = context();
+  const subsriptionReady = [Meteor.subscribe('user.current').ready()];
+  const dataReady = ()=> {
+    const userPermissions = (Meteor.user()) ? (Meteor.user().profile) ? (Meteor.user().profile.permissions) ? Meteor.user().profile.permissions : [] : [] : [];
+    onData(null, {userPermissions});
+  };
+  (subsriptionReady) ? dataReady() : onData();
+};
+
+export const depsMapper = (context, actions) => ({
+  context: () => context
+});
+
+export default composeAll(
+  composeWithTracker(composer),
+  useDeps(depsMapper)
+)(Team);

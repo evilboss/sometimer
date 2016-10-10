aa6762ee8ee5f39a6c1108c749217e97409da7ac
Jr Reyes <jr.evilboss@gmail.com>
2016-10-10 20:34:15 +0800

    Add usability to subtabs

--- app/client/modules/team/components/sub_tabs.jsx
+++ app/client/modules/team/components/sub_tabs.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-
+import {control} from '/lib/access-control/control';
 class SubTabs extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-   
+    const {target, text, permission, userPermissions} = this.props;
     return (
       <div className="row no-margin-bottom">
         <div className="col s12 no-padding">
-
-          <div className="btn-add">
-            <a href="" className="waves-effect waves-light secondary-color">
-              <span>Add New Team</span>
-              <i className="material-icons">add</i></a>
-          </div>
+          {
+            (userPermissions) ? control.isPermitted(permission, userPermissions) ?
+              <div className="btn-add">
+                <a href={(target)?target:''} className="waves-effect waves-light secondary-color">
+                  <span>{(text) ? text : ''}</span>
+                  <i className="material-icons">add</i></a>
+              </div>
+              : '' : ''
+          }
 
         </div>
       </div>

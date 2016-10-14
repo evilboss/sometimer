ddd391ba30f8cf5d9c8cf294392b70b4ab929c24
aaron <aaronrandrup@gmail.com>
2016-10-14 17:56:04 +0800

    fix staff settings layout add new field in staff settings

--- app/client/modules/team/components/manage_staff/add_new_staff.jsx
+++ app/client/modules/team/components/manage_staff/add_new_staff.jsx
@@ -52,93 +52,114 @@ class AddNewStaff extends React.Component {
                   <div className="col s12">
                     {
                       (userPermissions) ? control.isPermitted('updatePermissions', userPermissions) ?
-                    <table>
-                      <thead>
-                      <tr>
-                        <th>Set Permissions</th>
-                        <th className="center">View</th>
-                        <th className="center">Manage</th>
-                        <th className="center">Invite</th>
-                      </tr>
-                      </thead>
-                      <tbody>
-                      <tr>
-                        <td>Clients</td>
-                        <td className="center">
-                          <input type="checkbox" id="client-view"/>
-                          <label htmlFor="client-view"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="client-manage"/>
-                          <label htmlFor="client-manage"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="client-invite"/>
-                          <label htmlFor="client-invite"></label>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Staff</td>
-                        <td className="center">
-                          <input type="checkbox" id="staff-view"/>
-                          <label htmlFor="staff-view"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="staff-manage"/>
-                          <label htmlFor="staff-manage"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="staff-invite"/>
-                          <label htmlFor="staff-invite"></label>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Managers</td>
-                        <td className="center">
-                          <input type="checkbox" id="managers-view"/>
-                          <label htmlFor="managers-view"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="managers-manage"/>
-                          <label htmlFor="managers-manage"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="managers-invite"/>
-                          <label htmlFor="managers-invite"></label>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Executive Team</td>
-                        <td className="center">
-                          <input type="checkbox" id="executive-view"/>
-                          <label htmlFor="executive-view"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="executive-manage"/>
-                          <label htmlFor="executive-manage"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="executive-invite"/>
-                          <label htmlFor="executive-invite"></label>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Team Leaders</td>
-                        <td className="center">
-                          <input type="checkbox" id="leaders-view"/>
-                          <label htmlFor="leaders-view"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="leaders-manage"/>
-                          <label htmlFor="leaders-manage"></label>
-                        </td>
-                        <td className="center">
-                          <input type="checkbox" id="leaders-invite"/>
-                          <label htmlFor="leaders-invite"></label>
-                        </td>
-                      </tr>
-                      </tbody>
-                    </table>
+                        <table>
+                          <thead>
+                          <tr>
+                            <th>Set Permissions</th>
+                            <th className="center">View</th>
+                            <th className="center">Add</th>
+                            <th className="center">Edit</th>
+                            <th className="center">Delete</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          <tr>
+                            <td>Clients</td>
+                            <td className="center">
+                              <input type="checkbox" id="client-view"/>
+                              <label htmlFor="client-view"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="client-add"/>
+                              <label htmlFor="client-add"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="client-edit"/>
+                              <label htmlFor="client-edit"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="client-delete"/>
+                              <label htmlFor="client-delete"></label>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Staff</td>
+                            <td className="center">
+                              <input type="checkbox" id="staff-view"/>
+                              <label htmlFor="staff-view"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="staff-add"/>
+                              <label htmlFor="staff-add"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="staff-edit"/>
+                              <label htmlFor="staff-edit"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="staff-delete"/>
+                              <label htmlFor="staff-delete"></label>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Managers</td>
+                            <td className="center">
+                              <input type="checkbox" id="managers-view"/>
+                              <label htmlFor="managers-view"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="managers-view"/>
+                              <label htmlFor="managers-add"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="managers-edit"/>
+                              <label htmlFor="managers-edit"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="managers-delete"/>
+                              <label htmlFor="managers-delete"></label>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Executive Team</td>
+                            <td className="center">
+                              <input type="checkbox" id="executive-view"/>
+                              <label htmlFor="executive-view"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="executive-add"/>
+                              <label htmlFor="executive-add"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="executive-edit"/>
+                              <label htmlFor="executive-edit"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="executive-delete"/>
+                              <label htmlFor="executive-delete"></label>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>Team Leaders</td>
+                            <td className="center">
+                              <input type="checkbox" id="leaders-view"/>
+                              <label htmlFor="leaders-view"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="leaders-add"/>
+                              <label htmlFor="leaders-add"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="leaders-edit"/>
+                              <label htmlFor="leaders-edit"></label>
+                            </td>
+                            <td className="center">
+                              <input type="checkbox" id="leaders-delete"/>
+                              <label htmlFor="leaders-delete"></label>
+                            </td>
+                          </tr>
+                          </tbody>
+                        </table>
                         : '' : ''
                     }
                     <div className="right save">

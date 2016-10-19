73924fd9f534e55d2e0fead5f29363ae9f3e7f1f
aaron <aaronrandrup@gmail.com>
2016-10-19 19:05:17 +0800

    add new staff component partial update staff

---
+++ app/client/modules/staff/components/tests/permission_form.js
@@ -0,0 +1,8 @@
+const {describe, it} = global;
+import {expect} from 'chai';
+import {shallow} from 'enzyme';
+import PermissionForm from '../permission_form';
+
+describe('staff.components.permission_form', () => {
+  it('should do something');
+});

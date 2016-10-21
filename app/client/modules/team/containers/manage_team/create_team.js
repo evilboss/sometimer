9fffeb7de637405ac6ab6a2671b1a3119b33cb14
aaron <aaronrandrup@gmail.com>
2016-10-21 18:29:15 +0800

    add new npm module react-material-select

--- app/client/modules/team/containers/manage_team/create_team.js
+++ app/client/modules/team/containers/manage_team/create_team.js
@@ -1,11 +1,16 @@
 import {useDeps, composeAll, composeWithTracker, compose} from 'mantra-core';
 
 import CreateTeam from '../../components/manage_team/create_team.jsx';
-/*TODO: @aaron add new team */
+
 export const composer = ({context}, onData) => {
   const {Meteor, Collections} = context();
-  const team = Collections.Team;
-  onData(null, {team});
+  let subscriptionsReady = [Meteor.subscribe('users.allstaff').ready(), Meteor.subscribe('team.list').ready()];
+  const dataReady = ()=> {
+    let team = Collections.Team;
+    let allStaff = Meteor.users.find({'profile.role': 'staff'}).fetch();
+    onData(null, {allStaff, team});
+  };
+  (subscriptionsReady) ? dataReady() : onData();
 };
 
 export const depsMapper = (context, actions) => ({

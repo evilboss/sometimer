56b295d8ab94fe337fc69f92ea64abe373a2e994
Jr Reyes <jr.evilboss@gmail.com>
2016-11-26 10:13:08 +0800

    fix logic on teamlist

--- app/client/modules/team/components/manage_team/team_list.jsx
+++ app/client/modules/team/components/manage_team/team_list.jsx
@@ -15,7 +15,6 @@ class TeamList extends React.Component {
   }
 
 
-
   render() {
     const {currentUser, teamList, userPermissions} = this.props;
     return (
@@ -43,25 +42,40 @@ class TeamList extends React.Component {
                               }
                             </div>
                             <div className="card-content">
-                              <DisplayManager userId={team.teamLeader}
-                                              target={(userPermissions) ? control.isPermitted('updateManagers', userPermissions) ?`/dashboard/staff/settings/${team._id}/${team.teamLeader}`:'':''}/>
-                            </div>
-                            <div className="card-action">
-                              {(team.members) ?
-                                team.members.map((member, key)=>(
-                                  (key <= 4) ?
-                                    <StaffDp staffType="staff" key={key} teamId={team._id} userId={member}
-                                             target={ (userPermissions) ? control.isPermitted('updateStaffs', userPermissions) ?`/dashboard/staff/settings/${team._id}/${member}`:'':''}/> : ''
-                                )) : <div className="row no-margin">
+                              {(team.teamLeader) ?
+                                <DisplayManager userId={team.teamLeader}
+                                                target={(userPermissions) ? control.isPermitted('updateManagers', userPermissions) ? `/dashboard/staff/settings/${team._id}/${team.teamLeader}` : '' : ''}/>
+                                : <div className="row no-margin">
                                 <img
                                   src='/uploads/defaults/default_user.png'
                                   alt="Team Lead" className="circle responsive-img dp-small left"/>
                                 <div className="col s8 no-margin">
-                              <span>Needs a Staff!<br/>
-                              Manage now.
-                              </span>
+                                    <span>
+                                      Needs a Team Leader!<br/>Manage now.
+                                    </span>
                                 </div>
-                              </div>}
+                              </div>
+                              }
+
+                            </div>
+                            <div className="card-action">
+                              {(!_.isEmpty(team.members)) ?
+                                team.members.map((member, key)=>(
+                                  (key <= 4) ?
+                                    <StaffDp staffType="staff" key={key} teamId={team._id} userId={member} target={ (userPermissions) ? control.isPermitted('updateStaffs', userPermissions) ? `/dashboard/staff/settings/${team._id}/${member}` : '' : ''}/> : ''
+                                ))
+                                :
+                                <div className="row no-margin">
+                                  <img
+                                    src='/uploads/defaults/default_user.png'
+                                    alt="Team Lead" className="circle responsive-img dp-small left"/>
+                                  <div className="col s8 no-margin">
+                                    <span>Needs a Staff!<br/>
+                                    Manage now.
+                                    </span>
+                                  </div>
+                                </div>
+                              }
                             </div>
                           </div>
                         </article>

4ea2fc838621bd78c4f2b5d874cf72d2a5aefb12
Jr Reyes <jr.evilboss@gmail.com>
2016-12-30 09:35:50 +0800

    Fix loading stafflist while staff has not yet added

--- app/client/modules/staff/components/staff_details.jsx
+++ app/client/modules/staff/components/staff_details.jsx
@@ -35,65 +35,69 @@ class StaffDetails extends React.Component {
 
   render() {
     const {staff, index, userPermissions, currentUser, teamId, isStaff} = this.props;
-    const {role} = staff.profile;
     return (
-      (staff) ?
-        (staff.profile) ?
-          <tr key={index}>
-            <td className="staff-dp">
-              <a href={`/dashboard/staff-settings/team/${teamId}/${staff._id}`}>
-                <img
-                  src={(staff.profile.displayPhoto) ? staff.profile.displayPhoto : '/uploads/defaults/default_user.png'}
-                  alt="Staff"
-                  className="circle responsive-img dp-small"/>
-              </a>
+      <tr>
+        {
+          (staff) ?
+            (staff.profile) ?
+              <div>
+                <td className="staff-dp">
+                  <a href={`/dashboard/staff-settings/team/${teamId}/${staff._id}`}>
+                    <img
+                      src={(staff.profile.displayPhoto) ? staff.profile.displayPhoto : '/uploads/defaults/default_user.png'}
+                      alt="Staff"
+                      className="circle responsive-img dp-small"/>
+                  </a>
 
 
-            </td>
-            <td className="staff-details">
-              <div className="col no-margin">
+                </td>
+                <td className="staff-details">
+                  <div className="col no-margin">
 
-                <h6> {staff.profile.firstName}
-                </h6>
+                    <h6> {staff.profile.firstName}
+                    </h6>
+                  </div>
+                </td>
+                <td>
+                  <h6>{staff.profile.position}</h6>
+                </td>
 
+                <td className="center-align">
+                  {(isStaff) ? null :
+                    <StatusIndicator userId={staff._id} class={(staff.profile) ?
+                      (staff.profile.status) ? (staff.profile.status == 'completed') ? 'Out' : formatHelper.capitalize(staff.profile.status)
+                        : '' : ''}/>
+                  }
+
+                </td>
+
+                <td className="center-align">
+
+                  <div className="icons center-align">
+                    {(control.isAdmin(currentUser)) ?
+                      (userPermissions) ? control.isPermitted('updateStaffs', userPermissions) ?
+                        <a href='/dashboard/team/manage-managers'>
+                          <i className="material-icons">edit</i>
+                        </a>
+                        : '' : '' : ''
+                    }
+                    {
+                      (userPermissions) ? control.isPermitted('updateStaffs', userPermissions) ?
+                        <a href={`/dashboard/staff/${teamId}/${staff._id}`}>
+                          <img src="/Assets/icons/time.png"/>
+                        </a>
+                        : '' : ''
+                    }
+
+                  </div>
+
+                </td>
               </div>
-            </td>
-            <td>
-              <h6>{staff.profile.position}</h6>
-            </td>
+              : 'Please wait' : 'Please wait'
+        }
 
-            <td className="center-align">
-              {(isStaff) ? null :
-                <StatusIndicator userId={staff._id} class={(staff.profile) ?
-                  (staff.profile.status) ? (staff.profile.status == 'completed') ? 'Out' : formatHelper.capitalize(staff.profile.status)
-                    : '' : ''}/>
-              }
+      </tr>
 
-            </td>
-
-            <td className="center-align">
-
-              <div className="icons center-align">
-                {(control.isAdmin(currentUser)) ?
-                  (userPermissions) ? control.isPermitted('updateStaffs', userPermissions) ?
-                    <a href='/dashboard/team/manage-managers'>
-                      <i className="material-icons">edit</i>
-                    </a>
-                    : '' : '' : ''
-                }
-                {
-                  (userPermissions) ? control.isPermitted('updateStaffs', userPermissions) ?
-                    <a href={`/dashboard/staff/${teamId}/${staff._id}`}>
-                      <img src="/Assets/icons/time.png"/>
-                    </a>
-                    : '' : ''
-                }
-
-              </div>
-
-            </td>
-          </tr>
-          : '' : ''
     )
       ;
   }

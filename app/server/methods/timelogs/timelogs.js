ca58abd24db72083754e81ac7c7de3d3ef9149d9
evilboss <jr.evilboss@gmail.com>
2017-01-23 18:56:58 +0800

    fix date issue

--- app/server/methods/timelogs/timelogs.js
+++ app/server/methods/timelogs/timelogs.js
@@ -7,99 +7,103 @@ import {getHoursRendered} from '/server/methods/timeDate/timeDate';
  * @param status
  */
 const updateStatus = (status, userId, timelogId = null, breakId = null) => {
-  Meteor.users.update({_id: userId}, {
-    $set: {
-      'profile.status': status,
-      'profile.timeLogId': timelogId,
-      'profile.breakId': breakId
-    }
-  });
+    Meteor.users.update({_id: userId}, {
+        $set: {
+            'profile.status': status,
+            'profile.timeLogId': timelogId,
+            'profile.breakId': breakId
+        }
+    });
 };
-const getTimeLogId = (userId)=> {
-  return Meteor.users.findOne(userId).profile;
+const getTimeLogId = (userId) => {
+    return Meteor.users.findOne(userId).profile;
 };
 const startShift = () => {
-  const timeLog = {
-    userId: Meteor.userId(),
-    timeIn: new Date(),
-    createdAt: new Date(),
-    date: moment(new Date).format('DD:MM:YY'),
-    currentStatus: 'In',
-  };
-  const timelogId = Timelogs.insert(timeLog);
-  console.log(timelogId);
-  updateStatus('In', Meteor.userId(), timelogId);
+    let {timezone} = getTimeLogId(Meteor.userId());
+    let currentDate = new Date().toLocaleString("en-US", {timeZone: (timezone) ? timezone : "Asia/Manila"});
+    console.log('starting shift');
+    const timeLog = {
+        userId: Meteor.userId(),
+        timeIn: currentDate,
+        createdAt: currentDate,
+        date: moment(new Date().toLocaleString("en-US", {timeZone: (timezone) ? timezone : "Asia/Manila"})).format('DD:MM:YY'),
+        currentStatus: 'In',
+    };
+    const timelogId = Timelogs.insert(timeLog);
+    console.log(timelogId);
+    updateStatus('In', Meteor.userId(), timelogId);
 };
 const endShift = (userId) => {
-  const {timeLogId} = getTimeLogId(userId);
-  const currentLog = Timelogs.findOne(timeLogId);
-  const breakLogs = Breaks.find({userId: userId, timeLogId: timeLogId}).fetch();
-  const totalBreak = (_.isEmpty(breakLogs)) ? 0 : summation(breakLogs, 'duration');
-  const timeOut = new Date();
-  const totalRendered = (timeDiff(currentLog.timeIn, timeOut) - totalBreak);
-  Timelogs.update(timeLogId, {
-    $set: {
-      currentStatus: 'Out',
-      timeOut: timeOut,
-      totalBreak: totalBreak,
-      completed: true,
-      totalRendered: totalRendered
-    }
-  });
-  updateStatus('Out', userId);
+    const {timeLogId, timezone} = getTimeLogId(Meteor.userId());
+    const currentLog = Timelogs.findOne(timeLogId);
+    const breakLogs = Breaks.find({userId: userId, timeLogId: timeLogId}).fetch();
+    const totalBreak = (_.isEmpty(breakLogs)) ? 0 : summation(breakLogs, 'duration');
+    const timeOut = new Date().toLocaleString("en-US", {timeZone: (timezone) ? timezone : "Asia/Manila"});
+    const totalRendered = (timeDiff(currentLog.timeIn, timeOut) - totalBreak);
+    Timelogs.update(timeLogId, {
+        $set: {
+            currentStatus: 'Out',
+            timeOut: timeOut,
+            totalBreak: totalBreak,
+            completed: true,
+            totalRendered: totalRendered
+        }
+    });
+    updateStatus('Out', userId);
 };
 const startBreak = () => {
-  const {timeLogId} = getTimeLogId(Meteor.userId());
-  const breaklog = {
-    userId: Meteor.userId(),
-    timeLogId: timeLogId,
-    breakTimeIn: new Date(),
-    currentStatus: 'BreakIn'
-  };
-  const breakId = Breaks.insert(breaklog);
-  updateStatus('Break', Meteor.userId(), timeLogId, breakId);
+    const {timeLogId, timezone} = getTimeLogId(Meteor.userId());
+    let currentDate = new Date().toLocaleString("en-US", {timeZone: (timezone) ? timezone : "Asia/Manila"});
+
+    const breaklog = {
+        userId: Meteor.userId(),
+        timeLogId: timeLogId,
+        breakTimeIn: currentDate,
+        currentStatus: 'BreakIn'
+    };
+    const breakId = Breaks.insert(breaklog);
+    updateStatus('Break', Meteor.userId(), timeLogId, breakId);
 };
 const endBreak = (userId) => {
-  const {timeLogId, breakId} = getTimeLogId(userId);
-  const breakLog = Breaks.findOne(breakId);
-  const breakTimeOut = new Date();
-  Breaks.update(breakId, {
-    $set: {
-      currentStatus: 'BreakOut',
-      breakTimeOut: breakTimeOut,
-      duration: timeDiff(breakLog.breakTimeIn, breakTimeOut)
-    }
-  });
-  updateStatus('In', Meteor.userId(), timeLogId);
-
+    const {timeLogId, breakId} = getTimeLogId(userId);
+    const breakLog = Breaks.findOne(breakId);
+    const breakTimeOut = new Date().toLocaleString("en-US", {timeZone: (timezone) ? timezone : "Asia/Manila"});
+    Breaks.update(breakId, {
+        $set: {
+            currentStatus: 'BreakOut',
+            breakTimeOut: breakTimeOut,
+            duration: timeDiff(breakLog.breakTimeIn, breakTimeOut)
+        }
+    });
+    updateStatus('In', Meteor.userId(), timeLogId);
 };
 const approve = (timelogId) => {
-  const timelog = Timelogs.findOne(timelogId);
-  (timelog) ? Timelogs.update(timelog, {
-    $set: {approved: true}
-  }) : ''
+    const timelog = Timelogs.findOne(timelogId);
+    (timelog) ? Timelogs.update(timelog, {
+            $set: {approved: true}
+        }) : ''
 };
-const editLog = (timeIn, timeOut, timelog)=> {
-  const breakLogs = Breaks.find({userId: timelog.userId, timeLogId: timelog._id}).fetch();
-  const totalBreak = (_.isEmpty(breakLogs)) ? 0 : summation(breakLogs, 'duration');
-  const totalRendered = (timeDiff(timeIn, timeOut) - totalBreak);
-  (timelog) ? Timelogs.update(timelog, {
-    $set: {
-      timeIn: timeIn,
-      timeOut: timeOut,
-      totalBreak: totalBreak,
-      totalRendered: totalRendered
-    }
-  }) : ''
+const editLog = (timeIn, timeOut, timelog) => {
+    const breakLogs = Breaks.find({userId: timelog.userId, timeLogId: timelog._id}).fetch();
+    const totalBreak = (_.isEmpty(breakLogs)) ? 0 : summation(breakLogs, 'duration');
+    const totalRendered = (timeDiff(timeIn, timeOut) - totalBreak);
+    (timelog) ? Timelogs.update(timelog, {
+            $set: {
+                timeIn: timeIn,
+                timeOut: timeOut,
+                totalBreak: totalBreak,
+                totalRendered: totalRendered
+            }
+        }) : ''
 };
 const timelogs = {
-  startShift: () => startShift(),
-  endShift: (userId) => endShift(userId),
-  startBreak: () => startBreak(),
-  endBreak: (userId) => endBreak(userId),
-  approve: (timeLogId) => approve(timeLogId),
-  editLogs: (timeLogId, totalRendered) => editLogs(timeLogId, totalRendered),
-  editLog: editLog
+    startShift: () => startShift(),
+    endShift: (userId) => endShift(userId),
+    startBreak: () => startBreak(),
+    endBreak: (userId) => endBreak(userId),
+    approve: (timeLogId) => approve(timeLogId),
+    editLogs: (timeLogId, totalRendered) => editLogs(timeLogId, totalRendered),
+    editLog: editLog
 
 };
 export {timelogs};
--- app/server/migrations/migrations-index.js
+++ app/server/migrations/migrations-index.js
@@ -13,15 +13,15 @@
  * V9: Add client user stafflist
  * */
 
-const runMigrationsFromStart = ()=> {
-  Migrations.migrateTo(0);
-  Migrations.migrateTo('latest');
+const runMigrationsFromStart = () => {
+    Migrations.migrateTo(0);
+    Migrations.migrateTo('latest');
 };
 const runOnlyToLatest = () => {
-  Migrations.migrateTo('latest');
+    Migrations.migrateTo('latest');
 };
 Meteor.startup(function () {
-  //TODO: Stopped migrations from running
-  //(process.env.NODE_ENV === 'development') ? runMigrationsFromStart() : runOnlyToLatest();
-  runOnlyToLatest();
+    //TODO: Stopped migrations from running
+    //(process.env.NODE_ENV === 'development') ? runMigrationsFromStart() : runOnlyToLatest();
+    runOnlyToLatest();
 });

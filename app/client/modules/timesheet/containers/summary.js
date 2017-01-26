cdaa23def5c55985dc36924ed2e8e8981654a141
Jr Reyes <jr.evilboss@gmail.com>
2017-01-26 13:00:46 +0800

    Remove summary total function in summary container

--- app/client/modules/timesheet/containers/summary.js
+++ app/client/modules/timesheet/containers/summary.js
@@ -2,19 +2,12 @@ import {useDeps, composeAll, composeWithTracker, compose} from 'mantra-core';
 
 import Summary from '../components/summary.jsx';
 import {addTime} from '/lib/lib/time';
-const getSummaryTotal = (timelogs)=> {
-  let summaryTotal = '00:00:00';
-  _.each(timelogs, (log)=> {
-    summaryTotal = addTime(summaryTotal, log.totalRendered);
-  });
-  return summaryTotal;
-};
 export const composer = ({context, team, teamId, from, to}, onData) => {
   const {Meteor, Collections} = context();
   const subscriptionReady = [Meteor.subscribe('timelogs.team.approved', teamId, from, to).ready];
   const dataReady = () => {
     let summaryList = [];
-    _.each(team.members, (staffId)=> {
+    _.each(team.members, (staffId) => {
       const timelogs = Collections.Timelogs.find({userId: staffId, approved: true}).fetch();
       summaryList.push({
         _id: staffId, timelogs: timelogs, totalBreak: "00:00:00", totalHours: "00:00:00"
